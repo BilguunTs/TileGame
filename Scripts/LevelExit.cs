814ca850450490531fed452004722d9ca35778f9
BilguunTs <usosketch@yahoo.com>
2021-03-21 12:12:33 +0800

    adding menu

---
+++ Scripts/LevelExit.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelExit : MonoBehaviour
+{
+    // Start is called before the first frame update
+    [SerializeField] float LevelLoadDelay = 2f;
+    [SerializeField] float LevelExitSlowMoFactor = 0.2f;
+    IEnumerator LoadNextLevel()
+    {
+        Time.timeScale = LevelExitSlowMoFactor; 
+        yield return new WaitForSecondsRealtime(LevelLoadDelay);
+        Time.timeScale = 1f;
+        var currentIndex = SceneManager.GetActiveScene().buildIndex;
+        SceneManager.LoadScene(currentIndex+1);
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.gameObject.name == "Player")
+        {
+            StartCoroutine(LoadNextLevel());
+        }
+    }
+}
